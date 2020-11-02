8a93d0eeaef0ac9ce7beada8d1923538ba715d10
Kim Örnberg <kim.ornberg@skola.botkyrka.se>
2020-11-02 15:51:58 +0100

    guess

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Read instructions. And remove this line.
+
+function randomInt() {
+    let correctnumber  = Math.floor(Math.random() * 10)
+    return correctnumber 
+}
+
+
+function getUserGuess() {
+    const stringValue = document.getElementById('user-input').value
+    return parseInt(stringValue, 10)
+}
+
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const guess = getGuess()
+        return guess
+    }
+})
+
+
+
+
+    while (addEventListener != randomInt) 
+    {
+
+        if (addEventListener < randomInt)
+        {
+            alert ("That was wrong, its to low")
+            document.getElementById('user-input').value = ''
+        }
+        else if (getUserGuess > randomInt)
+        {
+            alert ("That was wrong, its to high")
+            document.getElementById('user-input').value = ''
+        }
+    }
+
+alert ("That was right")
